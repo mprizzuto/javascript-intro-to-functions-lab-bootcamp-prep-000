5e5259ab5091b2b91b5ddbd71bef2cc5770898c4
MARCO RIZZUTO <mprizzuto88@gmail.com>
2019-09-22 17:40:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string){
 
  function sayHiToGrandma(string){
    return 
-   string.toLowerCase();
-   string.toUpperCase();
+   string.toLowerCase()
+   string.toUpperCase()
 sayHiToGrandma==='I love you, too.';
  }
